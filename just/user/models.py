aec356922f08685a4f84bc28fcd131762d95fbce
Anandappu598 <23mcad16@kristujayanti.com>
2025-10-28 10:41:40 +0530

    just made a change

--- just/user/models.py
+++ just/user/models.py
@@ -5,3 +5,4 @@ class Usermodel(models.Model):
    username = models.CharField(null = True)
    email = models.CharField(blank = False)
    address = models.CharField(max_length= 150)
+   updated_at = models.DateTimeField(auto_now=True)
